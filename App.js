5aeb12e054af92206d82197a5a2a75f64b266a1d
Prince Velarde <53701801+Cha-er-si@users.noreply.github.com>
2022-06-04 15:19:41 +0800

    Changes

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaView, TextInput } from "react-native-web";
+import colors from "./components/colors";
 import Header from "./components/Header";
 
 export default function App() {
@@ -9,10 +10,10 @@ export default function App() {
       <Header />
       <View style={styles.content}>
         <View style={styles.gasLevel}>
-          <Text>Gas Level</Text>
+          <Text style={styles.title}>Gas Level</Text>
         </View>
         <View style={styles.gasContent}>
-          <Text>LPG Gas Content</Text>
+          <Text style={styles.title}>LPG Gas Content</Text>
         </View>
         <View style={styles.camera}>
           <Text>Camera IP Address</Text>
@@ -32,24 +33,29 @@ const styles = StyleSheet.create({
     margin: 10,
   },
   gasLevel: {
-    backgroundColor: "#CCD5AE",
+    backgroundColor: colors.lightGray,
     borderRadius: 20,
     height: "30%",
     margin: 10,
     padding: 15,
   },
   gasContent: {
-    backgroundColor: "#CCD5AE",
+    backgroundColor: colors.lightGray,
     borderRadius: 20,
     height: "30%",
     margin: 10,
     padding: 15,
   },
   camera: {
-    backgroundColor: "#CCD5AE",
+    backgroundColor: colors.lightGray,
     height: "25%",
     margin: 10,
     borderRadius: 20,
     padding: 15,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 20,
+    color: colors.black,
   },
 });
---
+++ components/colors.js
@@ -0,0 +1,11 @@
+export default {
+  gray: "#CCD5AE",
+  lightGray: "#E9EDC9",
+  milkyWhite: "#FEFAE0",
+  black: "#000",
+  white: "#fff",
+  cream: "FAEDCD",
+  brown: "#D4A373",
+  red: "#901B22",
+  green: "#778B3B",
+};
